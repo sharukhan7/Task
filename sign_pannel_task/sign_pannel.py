fbc504b445e475270fe0f274bd1ec89966c01588
sharukhan7 <sharukhan.s@ionixxtech.com>
2022-05-16 15:48:22 +0530

    sign pannel

--- sign_pannel_task/sign_pannel.py
+++ sign_pannel_task/sign_pannel.py
@@ -2,36 +2,29 @@
 requirment:
 pip install html2image
 '''
-import os
 from html2image import Html2Image
 
-def Html_to_Image(file,width,height,output_path):
-    hti = Html2Image()
-    hti.output_path = output_path
-    hti.screenshot(url='file://{}'.format(file),save_as='test_op6.png',size=(width,height))
+
 
 
 def replace_id(image,unique_id):
     with open(r'resource/file.html', 'r') as file:
         data = file.read()
-        #print(data)
         data = data.replace("src", image)
-        #print(data)
-        data = data.replace("unique_id", unique_id)
-        
+        data = data.replace("unique_id", unique_id)   
     with open(r'resource/temp.html', 'w') as file:
         file.write(data)
-        Html_to_Image(html,w,h,path)
+        
+        
 
+def Html_to_Image():
+    hti = Html2Image()
+    hti.output_path = 'resource/media/'
+    hti.screenshot(url='resource/temp.html',save_as='test_op6.png',size=(200,100))
 
-w=200
-h=100
-html="/home/sharukhan/Ionixx/Django/task/testing/test/resource/temp.html"
-path='/home/sharukhan/Ionixx/Django/task/testing/test/media'
-#Html_to_Image(html,w,h,path)
 
 img_src='src ="https://storage.googleapis.com/blockesign_staging/InkPaper__05_10_2022_09_04_57_360749"'
-unq_id='sdjkfhb4brbsnbfjhjskartvc'
+unq_id='sdjkfhb4brbsnbfjhjskartvck'
 replace_id(img_src,unq_id)
-
+Html_to_Image()
 
